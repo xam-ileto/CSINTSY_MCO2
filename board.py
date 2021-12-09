fe21269adda8885c3322be31d3baafe32e49fcc7
Maxine Ileto <xam.ileto@gmail.com>
2021-12-10 02:52:01 +0800

    added board and piece

---
+++ board.py
@@ -0,0 +1,5 @@
+class board:
+    def __init__(self, *args):
+        pass
+    # if user passes "start", then call _init_start
+    # else (if user passes a board), set board to be this
---
+++ piece.py
@@ -0,0 +1,18 @@
+class piece:
+    def __init__(self):
+        pass
+
+    def move():
+        pass
+
+    def can_move():
+        pass
+
+    def get_next_tiles():
+        # returns list of next possible immediate tiles (not taking into account skips)
+        pass
+    
+
+    def _is_tile_valid():
+        # used in get_next_tiles to see if an immediate tile will go out of bounds
+        pass
