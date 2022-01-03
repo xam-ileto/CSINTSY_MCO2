d1abb8ba94ccf5107008ae0ed52dad9d584a59f4
Maxine Ileto <xam.ileto@gmail.com>
2022-01-03 18:52:02 +0800

    coded tree init

--- Tree.py
+++ Tree.py
@@ -1,7 +1,19 @@
 from AiNode import AiNode
 
 class Tree:
-    def __init__(self, root, depth):
+    def __init__(self, root):
         self.root = root
         self.move_ordering_counter = 0
+        
+        # creates a tree with a depth of 3 (based on MP specs)
+        root.add_children()
+        
+        for node in root.children:
+            node.add_children()
+    
+    def minimax(node, depth, alpha, beta, maximizer, has_move_ordering):
+        pass
+    
+    def print_tree():
+        pass
         
