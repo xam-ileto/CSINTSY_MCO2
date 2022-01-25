42b62bca05ce4a1439452b16dea14e473d41f4a0
Maxine Ileto <xam.ileto@gmail.com>
2022-01-25 21:07:43 +0800

    made simulate turn have move ordering by default

--- Game.py
+++ Game.py
@@ -24,23 +24,23 @@ class Game:
             print("AI is calculating next move...")
             
             
-            root = AiNode(self.current_board, 0, "Red", None)
-            no_ordering_tree = Tree(root, 2)
-            move = no_ordering_tree.minimax(root, 0, -10000, 10000, "White", False)
-            visited = []
-            no_ordering_tree.count_nodes(visited,no_ordering_tree.root)
-            print("nodes generated: " + str(no_ordering_tree.number_of_nodes))
-            print("Without move ordering: " + str(no_ordering_tree.counter))
-            visited = []
-            no_ordering_tree.print_tree(visited, no_ordering_tree.root)
+            # root = AiNode(self.current_board, 0, "Red", None)
+            # no_ordering_tree = Tree(root, 2)
+            # move = no_ordering_tree.minimax(root, 0, -10000, 10000, "White", False)
+            # visited = []
+            # no_ordering_tree.count_nodes(visited,no_ordering_tree.root)
+            # print("nodes generated: " + str(no_ordering_tree.number_of_nodes))
+            # print("Without move ordering: " + str(no_ordering_tree.counter))
+            # visited = []
+            # no_ordering_tree.print_tree(visited, no_ordering_tree.root)
             
             
-            print('-----MOVE ORDERING---------')
+            # print('-----MOVE ORDERING---------')
             root = AiNode(self.current_board, 0, "Red", None)
             ordering_tree = Tree(root, 2)
             move = ordering_tree.minimax(root, 0, -10000, 10000, "White", True)
-            visited = []
-            ordering_tree.print_tree(visited,ordering_tree.root)
+            # visited = []
+            # ordering_tree.print_tree(visited,ordering_tree.root)
             visited = []
             ordering_tree.count_nodes(visited,ordering_tree.root)
             print("nodes generated: " + str(ordering_tree.number_of_nodes))
