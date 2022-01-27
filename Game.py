2205da44cad776adf8ba7dced040cab89f1099de
Maxine Ileto <xam.ileto@gmail.com>
2022-01-27 14:27:51 +0800

    moved main to game

--- Game.py
+++ Game.py
@@ -8,6 +8,28 @@ class Game:
         self.current_board = Board("Start")
         self.player_turn = "Red"
     
+    def game(self):
+        # show game instructions
+        print("Welcome to the game of checkers!")
+        print("\nHere are the mechanics:")
+        print("1. You (the player) are playing for color red, while the AI is playing for color white")
+        print("2. The board is represented below, with the names of the pieces written in a specific format. The letter symbolizes the color (R= Red, W= White)")
+        print("3. Once a piece reaches the end of the board, a K will be appended to its name, signifying that it is a king piece")
+        print("4. As per the rules of the standard checkers game, all jumps are forced")
+        print("\nGood luck!\n")
+
+        while self.current_board.check_game_over(self.player_turn) == False:
+            self.current_board.print_board()
+            
+            self.simulate_turn(self.player_turn)
+            self.change_turn()
+
+        # game over
+        if (self.player_turn == "White"):
+            print("Red wins!")
+        else:
+            print("White wins!")
+    
     def change_turn(self):
         '''Changes turn to either red or white'''
         if self.player_turn == "Red":
