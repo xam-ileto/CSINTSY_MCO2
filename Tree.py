48c7af64ae9b61a70a10f185c4efeb3b5db54f6d
Maxine Ileto <xam.ileto@gmail.com>
2022-01-29 19:14:05 +0800

    removed minimax with purning

--- Tree.py
+++ Tree.py
@@ -66,58 +66,6 @@ class Tree:
                 
             return minEval_node, minEval
     
-    def minimax_no_pruning(self, node, depth, maximizer, has_move_ordering):
-        '''performs the minimax algorithm without pruning and builds the tree as minimax is being performed'''
-        # turn is either "White" or "Red" depending on whose turn it is
-        if maximizer:
-            turn = "White"
-        else:
-            turn = "Red"
-        
-        
-        # generate children if node is non-leaf
-        if node.depth < self.max_depth:
-            node.add_children()
-            
-            # if move ordering is specified, sort the children
-            if has_move_ordering:
-                node.sort_children_descending()
-        self.counter += 1
-            
-        if depth == 0 or node.board.check_game_over(turn):
-            return node, node.score
-        
-        if maximizer:
-            maxEval = float('-inf')
-            maxEval_node = node
-            # counter = 0
-            for child in node.children:
-                eval_node, score = self.minimax_no_pruning(child, depth - 1, False, has_move_ordering)
-                # counter += 1
-                
-                original_maxEval = maxEval
-                maxEval = max(maxEval, score)
-                
-                if maxEval == score and maxEval != original_maxEval:
-                    maxEval_node = eval_node
-                
-                
-            return maxEval_node, maxEval
-        else: # if turn == "Red"
-            minEval = float('inf')
-            minEval_node = node
-            for child in node.children:
-                
-                eval_node, score = self.minimax_no_pruning(child, depth - 1, True, has_move_ordering)
-                original_minEval = minEval
-                minEval = min(minEval, score)
-                
-                if minEval == score and minEval != original_minEval:
-                    minEval_node.node = eval_node
-                
-                
-            return minEval_node, minEval
-    
     def count_nodes(self, visited_nodes, node):
         '''counts the total number of nodes'''
         if node not in visited_nodes:
